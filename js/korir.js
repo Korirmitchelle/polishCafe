0689bad63c9a9b15f9699ec3ab5b48ec364f4a0e
Korirmitchelle <korirmitchelle@gmail.com>
2016-06-30 18:11:29 +0300

    javascript

---
+++ js/korir.js
@@ -0,0 +1,24 @@
+function displayNextImage() {
+    x = (x === images.length -
+        1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+
+function
+displayPreviousImage() {
+    x = (x <= 0) ?
+        images.length - 1 : x - 1;
+    document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+    setInterval
+        (displayNextImage, 6000);
+}
+var images = [],
+    x = -1;
+images[0] = "images/10.jpg";
+images[1] = "images/burger-1156564_640.jpg";
+images[2] = "images/burger-868145_640.jpg";
+images[2] = "images/hamburger-1414423_640.jpg";
+images[2] = "images/food.jpeg";
